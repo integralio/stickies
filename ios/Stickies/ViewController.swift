0423a829ec6dea613beb861306ea2861fe61a5b6
Stefan Arambasich <sarambasich@gmail.com>
2018-10-01 16:28:46 -0400

    WIP [Create note] Adds the iOS directory and the Xcode project.
    TODO clean up these notes copied from Google Doc:
    
    Create new Xcode project
    Select Single View App
    “Stickies”, “Integral”, “io.integral” as identifier
    Swift
    Include Unit and UI tests
    out in new “ios” directory, no new git repo
    Select iphone 8
    Build and run (ignore team warning)
    Commit
    TODO disable project orientations

---
+++ ios/Stickies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stickies
+//
+//  Created by Stefan on 10/1/18.
+//  Copyright © 2018 Integral. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
