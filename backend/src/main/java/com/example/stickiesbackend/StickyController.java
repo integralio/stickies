38e97717b14c23ed5de89035a1ac63189c19a45d
Steve Anderson <steve@integral.io>
2018-10-02 11:24:44 -0400

    WIP [Persist note] Add a test for getting a 200 on GET /sticky
    Signed-off-by: Xing Zhou <xing@integral.io>

---
+++ backend/src/main/java/com/example/stickiesbackend/StickyController.java
@@ -0,0 +1,14 @@
+package com.example.stickiesbackend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sticky")
+public class StickyController {
+    @GetMapping
+    public void index() {
+
+    }
+}
--- backend/src/test/java/com/example/stickiesbackend/StickiesBackendApplicationTests.java
+++ backend/src/test/java/com/example/stickiesbackend/StickiesBackendApplicationTests.java
@@ -2,15 +2,31 @@ package com.example.stickiesbackend;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class StickiesBackendApplicationTests {
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     public void contextLoads() {
     }
 
+    @Test
+    public void responseContainsEmptyArray_whenThereAreNoStickies() throws Exception {
+        mvc.perform(get("/sticky")).andExpect(status().isOk());
+    }
+
 }
+
