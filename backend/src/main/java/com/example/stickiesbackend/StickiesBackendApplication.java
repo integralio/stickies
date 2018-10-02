ae67b07d72102d890485b333664f8adc26f807e9
integral <careers@integral.io>
2018-10-02 10:58:52 -0400

    WIP [Persist note] init gradle project

---
+++ backend/src/main/java/com/example/stickiesbackend/StickiesBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.stickiesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickiesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StickiesBackendApplication.class, args);
+    }
+}
